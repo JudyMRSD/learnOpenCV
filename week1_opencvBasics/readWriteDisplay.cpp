ddbbcba188d9d33b1f489171c244e5870182bc00
Jin Zhu <jinz1@andrew.cmu.edu>
2018-01-26 10:06:17 -0500

    display image

--- week1_opencvBasics/readWriteDisplay.cpp
+++ week1_opencvBasics/readWriteDisplay.cpp
@@ -1,6 +1,20 @@
-#include <iostream>
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+using namespace cv;
+using namespace std;
+// Mat as image container
+void display(){
+    string filePath = "../images/sample.jpg";
+    Mat image  = imread(filePath, IMREAD_COLOR);
+    namedWindow( "Display window", WINDOW_AUTOSIZE );
+    imshow("Display Image", image);
+
+    waitKey(0);
+
+}
+int main()
+{
+    display();
     return 0;
 }
